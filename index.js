665b131fcab6936ab413ac50b8852212f143a7f5
William Gotterer <william.gotterer@gmail.com>
2021-09-22 10:34:27 -0400

    commit tree 84a8c884daf254ee77161fb54a5beb3876b32d6b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+
+  function superbowlWin(array){
+      for (const record of array){
+      if ( record.result === "W"){
+          return record.year
+      }
+      }
+
+  }
+
+  record.find(superbowlWin);
